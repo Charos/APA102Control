ecc6cee0273dce56e20618fa2fd715447d231850
Charos <kohll6@bfh.ch>
2016-04-05 14:40:21 +0200

    jni Files added

---
+++ app/src/main/java/ch/bfh/ti/apa102control/SPI.java
@@ -0,0 +1,15 @@
+package ch.bfh.ti.apa102control;
+
+/**
+ * Created by student on 05.04.16.
+ */
+public class SPI {
+    public native int open(String deviceName);
+    public native int read(int fileHandler, int buffer[], int length);
+    public native int write(int fileHandler, int buffer[], int length);
+    public native int close(int fileHandler);
+    static
+    {
+        System.loadLibrary("spi");
+    }
+}
