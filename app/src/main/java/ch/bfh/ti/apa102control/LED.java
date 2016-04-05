4de6217338ce8b405950da90c660268fac7bcd32
Micha Studer <micha.studer@bbcag.ch>
2016-04-05 14:27:38 +0200

    added Class LED

---
+++ app/src/main/java/ch/bfh/ti/apa102control/LED.java
@@ -0,0 +1,34 @@
+package ch.bfh.ti.apa102control;
+
+import android.graphics.Color;
+
+import java.util.ArrayList;
+
+/**
+ * Created by micha on 05.04.16.
+ */
+public class LED {
+
+    static char[] LED2Buffer(ArrayList<LED> leds)
+    {
+
+        char[] buffer=new char[leds.size()*4+8];
+        for(int i=0;i<4;i++)
+        {
+            buffer[i]=0;
+        }
+        int i=0;
+        for (LED led:leds) {
+            buffer[4+i*4]=(char)Color.alpha(led.color);
+            buffer[5+i*4]=(char)Color.red(led.color);
+
+
+
+        }
+
+        }
+    }
+
+
+    public int color;
+}
